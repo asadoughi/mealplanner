1ce0c23ec66a755dae3501b51299c692569719c8
Amir Sadoughi <amir.sadoughi@gmail.com>
2014-01-23 22:19:35 -0600

    Updated meal_sr26abbr.py
    * Replaced grilled chicken with (pan-)fried chicken
    * Replaced cooked sockeye with dry-heat cooked sockeye
    * Replaced broccoli clusters with (entire) broccoli
    * Updated algorithm to total nutrients instead of only macros

--- meal_sr26abbr.py
+++ meal_sr26abbr.py
@@ -1,4 +1,4 @@
-from sr26abbr.load import load
+from sr26abbr import load
 
 meal_plan = [
     ("01132", 0.5),
@@ -8,39 +8,45 @@ meal_plan = [
     ("18076", 0.85),
     ("12061", 0.57),
     ("11457", 0.57),
-    ("05747", 1.13),
+    ("05063", 1.13),
     ("11529", 1.00),
     ("09037", 0.68),
     ("20037", 2.50),
     ("11124", 1.28),
     ("16158", 0.28),
     ("09202", 1.54),
-    ("15273", 1.42),
-    ("11740", 0.91),
+    ("15086", 1.42),
+    ("11090", 0.91),
     ("01256", 2.25),
 ]
 
 
-db = load()
-total_calories = 0
-total_protein = 0
-total_fat = 0
-total_carbs = 0
+db = load.load()
+total = dict()
 for meal, weight in meal_plan:
     i = db[meal]
-    kcal = i["Energ_Kcal"] * weight
-    protein = i["Protein"] * weight
-    fat = i["Lipid_Tot"] * weight
-    carbs = i["Carbohydrt"] * weight
-    print "%s:%s:%s:%s:%s" % (i["Shrt_Desc"], kcal, protein, fat, carbs)
-    total_calories += kcal
-    total_protein += protein
-    total_fat += fat
-    total_carbs += carbs
+    for field_name, field_type in load.FIELDS:
+        if field_type is str:
+            continue
+        if not i[field_name]:
+            continue
 
-print total_calories, total_protein, total_fat, total_carbs
-verify_total = total_protein * 4 + total_fat * 9 + total_carbs * 4
-print "Verify total kcal", verify_total
-print "Protein %.1f" % (total_protein * 4 / verify_total * 100.)
-print "Fat %.1f" % (total_fat * 4 / verify_total * 100.)
-print "Carbs %.1f" % (total_carbs * 4 / verify_total * 100.)
+        w = i[field_name] * weight
+        if field_name not in total:
+            total[field_name] = w
+        else:
+            total[field_name] += w
+
+total.pop("GmWt_1")
+total.pop("GmWt_2")
+for key in total:
+    print key, total[key]
+
+
+def print_meal_key(key):
+    total = 0
+    for num, weight in meal_plan:
+        item = db[num]
+        print item[key], item["Shrt_Desc"]
+        total += item[key] * weight if item[key] else 0
+    print total
