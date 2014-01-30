11f68db14f1df815619b06d61f313dff8364da5a
Amir Sadoughi <amir.sadoughi@gmail.com>
2014-01-29 20:06:28 -0600

    Added iterative user removal, sorted on nutrient density

--- knapsack.py
+++ knapsack.py
@@ -33,27 +33,37 @@ target = dict(
     Zinc=11,
     )
 
-db_nuts = {tkey: {} for tkey in target.keys()}
-for key in db.keys():
+while True:
+    db_nuts = {tkey: {} for tkey in target.keys()}
+    for key in db.keys():
+        cal = db[key]["Energ_Kcal"]
+        for tkey in target.keys():
+            db_nuts[tkey][key] = db[key][tkey] / cal if db[key][tkey] and cal else 0
+
+    sorted_nuts = dict()
     for tkey in target.keys():
-        db_nuts[tkey][key] = db[key][tkey] if db[key][tkey] else 0
+        sorted_nuts[tkey] = sorted(
+            db_nuts[tkey].iteritems(), reverse=True, key=lambda x: x[1])
 
-sorted_nuts = dict()
-for tkey in target.keys():
-    sorted_nuts[tkey] = sorted(
-        db_nuts[tkey].iteritems(), reverse=True, key=lambda x: x[1])
+    meal = []
+    for tkey in target.keys():
+        k, v = sorted_nuts[tkey][0]
+        weight = target[tkey] / (v * db[key]["Energ_Kcal"])
+        meal.append((k, weight))
 
-meal = []
-for tkey in target.keys():
-    k, v = sorted_nuts[tkey][0]
-    weight = target[tkey] / v
-    meal.append((k, weight))
+    sums = dict(Sugar_Tot=0, FA_Sat=0, Cholestrl=0, Energ_Kcal=0)
+    for item, weight in meal:
+        print item, "%.2f" % (weight * 100), db[item]["Shrt_Desc"]
+        for sum_key in sums.keys():
+            if db[item][sum_key]:
+                sums[sum_key] += db[item][sum_key] * weight
+    print sums
 
-
-sums = dict(Sugar_Tot=0, FA_Sat=0, Cholestrl=0, Energ_Kcal=0)
-for item, weight in meal:
-    print item, "%.2f" % (weight * 100), db[item]["Shrt_Desc"]
-    for sum_key in sums.keys():
-        if db[item][sum_key]:
-            sums[sum_key] += db[item][sum_key] * weight
-print sums
+    keycode = raw_input("Item code to remove: ")
+    print "Removing [%s]" % keycode
+    if len(keycode) <  5:
+        for key in db.keys():
+            if key.startswith(keycode):
+                db.pop(key)
+    else:
+        db.pop(keycode)
